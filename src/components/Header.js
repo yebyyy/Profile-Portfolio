85292640b8ee6297b4e9cdb19f3b2790f386a9e0
yebyyy <yexiaomeng2004@gmail.com>
2024-02-12 13:39:52 -0500

    started working on the profile page using tailwind, and tailwind.css is now innitialized

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="text-center p-10 bg-gray-100">
+            <img src="/images/Profilepic.jpeg" alt="Profile" className="rounded-full mx-auto max-w-xs max-h-xs w-16 h-16 object-cover" />
+            <h1 className="text-3xl font-bold mt-2">Xiaomeng Ye</h1>
+            <p className="text-gray-600">Software Developer</p>
+        </div>
+    );
+}
+export default Header;
